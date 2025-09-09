01efadab25130b1d25279ce4579c663cb50354f0
olivercaleb300 <olivercaleb300@gmail.com>
2025-09-08 23:03:16 -0400

    Updated schema.sql

--- employee_db/schema.sql
+++ employee_db/schema.sql
@@ -3,7 +3,7 @@
 -- ==========================
 CREATE TABLE departments (
     id SERIAL PRIMARY KEY,
-    name VARCHAR (100) UNIQUE NOT NULL,
+    name VARCHAR(100) UNIQUE NOT NULL,
     location VARCHAR(100),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     created_by VARCHAR(50),
@@ -11,68 +11,61 @@ CREATE TABLE departments (
     updated_by VARCHAR(50)
 );
 
-
 -- ==========================
 -- EMPLOYEES TABLE
 -- ========================== 
 CREATE TABLE employees (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL ,
-    department_id INT REFERENCES departments(id) ,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    department_id INT REFERENCES departments(id),
     role VARCHAR(50),
     hire_date DATE,
-    salary decimal(10,2) CHECK (SALARY >=0),
+    salary DECIMAL(10,2) CHECK (salary >= 0),
     is_active BOOLEAN DEFAULT TRUE,
-    created_at TIMESTAMP DEFAULT  CURRENT_TIMESTAMP,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     created_by VARCHAR(50),
-    updated_at TIMESTAMP DEFAULT  CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_by VARCHAR(50)
 );
-
 
 -- ========================== 
 -- PROJECTS TABLE
 -- ==========================
-
 CREATE TABLE projects ( 
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) UNIQUE NOT NULL,
     description TEXT,
     start_date DATE,
     end_date DATE CHECK (end_date >= start_date),
-    created_at TIMESTAMP DEFAULT  CURRENT_TIMESTAMP,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     created_by VARCHAR(50),
-    updated_at TIMESTAMP DEFAULT  CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_by VARCHAR(50)
 );
-
 
 -- ========================== 
 -- EMPLOYEE PROJECTS TABLE
 -- ==========================
-
 CREATE TABLE employee_projects (
     employee_id INT NOT NULL REFERENCES employees(id),
     project_id INT NOT NULL REFERENCES projects(id),
     assigned_date DATE,
-    PRIMARY KEY (employee_id , project_id)
+    PRIMARY KEY (employee_id, project_id)
 );
-
 
 -- ==========================
 -- USERS TABLE
 -- ==========================
-
 CREATE TABLE users (
-    id serial PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
     role VARCHAR(50),
-    employee_id INT NOT NULL REFERENCES employee(id),
+    employee_id INT NOT NULL REFERENCES employees(id),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     created_by VARCHAR(50),
-    updated_at TIMESTAMP DEFAULT  CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_by VARCHAR(50)
 );
 
